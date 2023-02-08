07feb037884213af4b554529dbbcffcc6708d4d1
Frank Schirrmeister <f.schirrmeister@telekom.de>
2023-02-08 12:32:53 +0100

    Task 1

---
+++ task1a.py
@@ -0,0 +1,13 @@
+import utils.utils as utils
+
+c = utils.read_file("files/task1a.txt")
+biggest_num = 0
+curr_num = 0
+for elem in c:
+    if elem[0].isnumeric(): curr_num += int(elem[0])
+    else:
+        if curr_num > biggest_num: biggest_num = curr_num
+        curr_num = 0
+print (f"Größte Numer = {biggest_num}") 
+    
+
---
+++ task1b.py
@@ -0,0 +1,17 @@
+import utils.utils as utils
+
+c = utils.read_file("files/task1.txt")
+biggest_num_field = [0,0,0]
+curr_num = 0
+for elem in c:
+    if elem[0].isnumeric(): curr_num += int(elem[0])
+    else:
+        if curr_num > biggest_num_field[0]: 
+            biggest_num_field[0] = curr_num
+            biggest_num_field.sort()
+        curr_num = 0
+biggest_num = 0
+for elem in biggest_num_field:
+    biggest_num += elem
+print (f"Größte Numer = {biggest_num}") 
+    
