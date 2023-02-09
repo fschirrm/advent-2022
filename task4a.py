5753c102e1233d7ca7e40f55c5b0fe0c8d30bec0
Frank Schirrmeister <f.schirrmeister@telekom.de>
2023-02-09 16:27:35 +0100

    Task 4a

---
+++ task4a.py
@@ -0,0 +1,16 @@
+import utils.utils as utils
+
+c = utils.read_file("files/task4.txt")
+count = 0
+for elem in c:
+    sp = elem[0].split(',')
+    l_range = sp[0].split('-')
+    r_range = sp[1].split('-')
+    if (int(l_range[0]) == int(r_range[0])) or (int(l_range[1]) == int(r_range[1])): count += 1
+    else:
+        if int(l_range[0]) < int(r_range[0]): 
+            if int(l_range[1]) > int(r_range[1]): count += 1
+        else: 
+            if int(r_range[1]) > int(l_range[1]): count += 1
+print(f"number of complete overlapping pairs: {count}")
+               
