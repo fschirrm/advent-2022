2d6c182f4807a95817aba71f4d02d7b9daf34501
Frank <f.schirrmeister@t-online.de>
2023-02-11 18:51:06 +0100

    Task 6b

---
+++ task6b.py
@@ -0,0 +1,19 @@
+def check_unique(s):
+    unique = True
+    for m in range(len(s)):
+        for n in range(len(s)):
+            if (m != n) and (s[m] == s[n]): 
+                unique = False
+                break
+        if unique == False: break
+    return unique    
+
+f = open("files/task6.txt","r")
+line = f.readline().strip()
+marker = 0
+for i in range(14, len(line)):
+    if check_unique(line[i-14:i]):
+        marker = i
+        break
+
+print(f"marker: {marker}")
